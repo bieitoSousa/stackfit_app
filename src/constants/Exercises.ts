c749c496a6ca34f9884fb97ae836ffcdb1b0db9c
bieito <bieito.sousa.1991@gmail.com>
2021-11-10 11:02:34 +0100

    update

---
+++ src/constants/Exercises.ts
@@ -0,0 +1,147 @@
+import React from 'react'
+
+export default  {
+    exercises : [
+        // WARN_UP
+        {
+            id:'DEEP_SQT_OH_ROT',
+            name :'Deep SQT. OH. ROT.',
+            longName:'Deep Squat Over Head Rotation',
+            type:'WARN_UP',
+            objective:'leg, glutes, quadriceps',
+            desc:'',
+            img:'https://media1.popsugar-assets.com/files/thumbor/hDbDJ9NvbLv_RAZ8Len555qDKYM/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/11/28/758/n/1922729/0ebc401d33e7088a_f8da1ac553afe76f_Squat-Overhead-Reach/i/Squat-Overhead-Reach.jpg',
+            level:0,
+            rating:0,
+
+        },
+        {
+            id:'FLOOR_SHOULDER_SLIDES',
+            name : 'Floor Shoulder Slides',
+            longName:'',
+            type:'WARN_UP',
+            objective:'scapular mobility, shoulder',
+            desc:'',
+            img:'https://i.ytimg.com/vi/z0__rFcjQXI/maxresdefault.jpg',
+            level:0,
+            rating:0,
+
+        },
+        {
+            id:'WALL_SHOULDER_SLIDES',
+            name : 'Wall Shoulder Slides',
+            longName:'',
+            type:'WARN_UP',
+            objective:'scapular mobility, shoulder',
+            desc:'',
+            img:'https://i.pinimg.com/474x/bb/91/8f/bb918f74a1e318796bc343360e2e6e53--pregnancy-workout-trimesters-of-pregnancy.jpg',
+            level:0,
+            rating:0,
+
+        },
+        {
+            id:'LUNGE',
+            name : 'Lunge',
+            longName:'',
+            type:'WARN_UP',
+            objective:'leg, buttock',
+            desc:'',
+            img:'https://www.estylum.com/wp-content/uploads/2019/01/JUMP-LUNGES.jpg',
+            level:0,
+            rating:0,
+
+        },
+        {
+            id:'SINGLE_LEG_DEADLIFT',
+            name : 'Single leg deadlift',
+            longName:'',
+            type:'WARN_UP',
+            objective:'leg, gluteus,lumbar,stability',
+            desc:'',
+            img:'https://i.imgur.com/J4ndzlH.jpeg',
+            level:0,
+            rating:0,
+
+        },
+        {
+            id:'Y_BALANCE',
+            name : 'Y Balance',
+            longName:'',
+            type:'WARN_UP',
+            objective:'balance, stability',
+            desc:'',
+            img:'https://www.sportlife.es/uploads/s1/76/15/89/9/article-valorar-mejorar-equilibrio-dinamico-552f923720471.jpeg',
+            level:0,
+            rating:0,
+
+        },
+
+          // ABS
+        {
+            id:'MOUNTAIN_CLIMBERS',
+            name : 'Mountain Climbers',
+            longName:'',
+            type:'ABS',
+            objective:'ABS',
+            desc:'',
+            img:'https://media.istockphoto.com/vectors/step-of-doing-the-mountain-climber-exercise-by-healthy-woman-vector-id957699448?k=20&m=957699448&s=612x612&w=0&h=kxdGeANir4ASYaCyuXkeHkoL-VZTPQ9eOIRKjuhuCWc=',
+            level:0,
+            rating:0,
+        },
+
+        // WHEIGHTLIFTING - TECHNIQUE
+        {
+            id:'PVC_SQUAT_THERAPY_FACE_WALL',
+            name : 'PVC squat therapy face wall',
+            longName:'',
+            type:'WHEIGHTLIFTING_TECHNIQUE',
+            objective:'OVERHEAD_SQUAT',
+            desc:'',
+            img:'',
+            level:0,
+            rating:0,
+        },
+        
+        // WHEIGHTLIFTING 
+        {
+            id:'OVERHEAD_SQUAT',
+            name : 'Mountain Climbers',
+            longName:'',
+            type:'WHEIGHTLIFTING',
+            objective:'',
+            desc:'',
+            img:'',
+            level:0,
+            rating:0,
+            variants:[{name :'Behind The Neck Push Press', img:'', desc:''}],
+            technique:['PVC_SQUAT_THERAPY_FACE_WALL'],
+        },
+        {
+            id:'DB_BOX_STEP_OVER',
+            name : 'DB BOX STEP OVER',
+            longName:'',
+            type:'',
+            objective:'',
+            desc:'',
+            img:'',
+            level:0,
+            rating:0,
+            variants:[{name :'Behind The Neck Push Press', img:'', desc:''}],
+            technique:['PVC_SQUAT_THERAPY_FACE_WALL'],
+        },
+        {
+            id:'DB_POWER_CLEAN_ALT',
+            name : 'DB power clean alt',
+            longName:'',
+            type:'',
+            objective:'',
+            desc:'',
+            img:'',
+            level:0,
+            rating:0,
+            variants:[{name :'Behind The Neck Push Press', img:'', desc:''}],
+            technique:['PVC_SQUAT_THERAPY_FACE_WALL'],
+        },
+
+    ]
+}
---
+++ src/constants/Sesions.ts
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export default {
+    sessions: [{
+        id:0,
+        name: '',
+        type: '',
+        date: '',
+        sessionBlock: [{
+            type: 'WARN_UP',
+            desc: '',
+            img: '',
+            timer: {
+                type: 'EMOM',
+                time: 5,
+                ud: 'min',
+                rest: 0,
+                cda: 0,
+            },
+            block: [{
+                reps: [],
+                exercise: [
+                    'DEEP_SQT_OH_ROT',
+                    'FLOOR_SHOULDER_SLIDES',
+                    'WALL_SHOULDER_SLIDES',
+                    'LUNGE',
+                    'SINGLE_LEG_DEADLIFT',
+                    'Y_BALANCE',
+                    'MOUNTAIN_CLIMBERS'
+                ]
+            }]
+        },
+        {
+            type: 'WHEIGHTLIFTING',
+            desc: '',
+            img: '',
+            timer: {
+                type: 'TIME',
+                time: 4,
+                ud: 'min',
+                rest: 0,
+                cda: 0,
+            },
+            block: [{
+                reps: [3],
+                exercise: ['OVERHEAD_SQUAT'],
+                desc : 'Behind The Neck Push Press',
+            }, {
+                reps: [3],
+                exercise: ['OVERHEAD_SQUAT'],
+                desc : 'Tempo (MAX WHEIGHT 30%)',
+            }
+            ]
+        },
+        {
+            type: 'WHEIGHTLIFTING',
+            desc: '',
+            img: '',
+            timer: {
+                type: 'EVERY',
+                time: 0,
+                rest: 0,
+                cda: '1:30',
+                rounds:7,
+                
+            },
+            block: [{
+                reps: [3],
+                exercise: ['OVERHEAD_SQUAT'],
+                desc : 'Increment (MAX WHEIGHT 70%)',
+            }, {
+                reps: [5],
+                exercise: ['PVC_SQUAT_THERAPY_FACE_WALL'],
+                desc : 'Therapy face wall',
+            }
+            ]
+        },
+        {
+            type: 'MET_CON',
+            desc: '',
+            img: '',
+            timer: {
+                type: 'FOR_TIME',
+                time: '20:00',
+                rest: '1:00',
+                cda: '',
+                rounds: 6,
+                
+            },
+            block: [{
+                reps: [16,16],
+                exercise: ['DB_BOX_STEP_OVER','DB_POWER_CLEAN_ALT'],
+            }]
+        },
+        ]
+    }]
+}
